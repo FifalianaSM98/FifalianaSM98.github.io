de00c731dbcb82b09a3ad0c2776c7db9783f8110
FifalianaSM98 <129393950+FifalianaSM98@users.noreply.github.com>
2023-04-03 04:52:58 +0100

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,35 @@
+let myHeading = document.querySelector('h1');
+myHeading.textContent = 'Bonjour, monde !';
+
+document.querySelector('html').addEventListener('click', function() {
+    alert('Aïe, arrêtez de cliquer !!');
+});
+
+let myImage = document.querySelector('img');
+
+myImage.addEventListener('click', function() {
+    let mySrc = myImage.getAttribute('src');
+    if (mySrc === 'images/psy1.jpeg') {
+      myImage.setAttribute('src', 'images/psy2.jpeg');
+    } else {
+      myImage.setAttribute('src', 'images/psy1.jpeg');
+    }
+});
+
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+function setUserName() {
+    let myName = prompt('Veuillez saisir votre nom.');
+    localStorage.setItem('nom', myName);
+    myHeading.textContent = `Parler, +${myName}`;
+  }
+  if (!localStorage.getItem('nom')) {
+    setUserName();
+  } else {
+    let storedName = localStorage.getItem('nom');
+    myHeading.textContent = 'Mozilla est cool, ' + storedName;
+  }
+  myButton.addEventListener('click', function() {
+    setUserName();
+  });
